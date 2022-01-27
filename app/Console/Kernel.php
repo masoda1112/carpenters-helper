2f5e7d9bce03f4ef7a54114968ec3c40b69f6829
masoda1112 <wendy.odakura@gmail.com>
2022-01-27 09:35:19 +0900

    タスクスケジューラdebug

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,8 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('send:line')->dailyAt('09:00');
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('send:line')->dailyAt('09:40');
     }
 
     /**
